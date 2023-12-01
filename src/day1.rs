01ee4db462c1bf53e9b6d30eeb4a9e198059d00a
Sam Jones <sj@bendingspoons.com>
2023-12-01 11:49:17 +0100

    feat: day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,29 @@
 pub fn solution(input: Vec<String>) -> String {
-    return String::from("Hello World!");
+    let mut result: u32 = 0;
+    for line in input {
+        let digits: Vec<char> = line
+            .chars()
+            .filter(|c| c.is_digit(10))
+            .collect();
+        let concat = format!("{}{}", digits.first().unwrap(), digits.last().unwrap());
+        result += concat.parse::<u32>().unwrap();
+    }
+    return result.to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        let input = vec![
+            String::from("1abc2"),
+            String::from("pqr3stu8vwx"),
+            String::from("a1b2c3d4e5f"),
+            String::from("treb7uchet")
+        ];
+        let result = solution(input);
+        assert_eq!(result, "142");
+    }
 }
