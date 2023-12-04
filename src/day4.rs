2543bb0183910a36993d4ccbe0d5ce7e6ea16f71
Sam Jones <sam.jones1007@gmail.com>
2023-12-04 06:22:04 +0100

    feat: day 4 part 1

---
+++ src/day4.rs
@@ -0,0 +1,47 @@
+pub fn solution(input: Vec<String>) -> (String, String) {
+    return (part1(&input), part2(&input));
+}
+
+fn part1(input: &Vec<String>) -> String {
+    let mut result = 0;
+    for line in input {
+        let parts: Vec<&str> = line.split(":").last().unwrap().split("|").collect();
+        let winning: Vec<&str> = parts[0].trim().split(" ").filter(|e| *e != "").collect();
+        let matches: u32 = parts[1].trim().split(" ").filter(|e| *e != "").filter(|e| winning.contains(e)).count().try_into().unwrap();
+        if matches > 0 {
+            result += 2_u32.pow(matches-1);
+        }
+    };
+
+    return result.to_string();
+}
+
+fn part2(input: &Vec<String>) -> String {
+    return String::new();
+}
+
+#[cfg(test)]
+mod test {
+    use super::solution;
+
+    #[test]
+    fn test_part1_basic() {
+        let input = vec![
+            String::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+            String::from("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+            String::from("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+            String::from("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+            String::from("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+            String::from("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+        ];
+
+        let (result, _) = solution(input);
+
+        assert_eq!(result, "13");
+    }
+
+    #[test]
+    fn test_part2_basic() {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{fs::{self}, env};
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 type Solution = fn(Vec<String>) -> (String, String);
 
@@ -15,6 +16,7 @@ fn main() {
         "1" => day1::solution,
         "2" => day2::solution,
         "3" => day3::solution,
+        "4" => day4::solution,
         _ => |_| (String::new(), String::new())
     };
     let (part1, part2) = solution(input);
